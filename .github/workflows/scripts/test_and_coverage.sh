3cf167cb8a6440c2cbd766e06ee9bd0c4d40aeee
Engineering Team <oc-engteam@codetrek.work>
2026-05-29 15:24:56 +0800

    chore: use shared go coverage tool

--- .github/workflows/scripts/test_and_coverage.sh
+++ .github/workflows/scripts/test_and_coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Run coverage tool in CI mode
 # This script is called by GitHub Actions workflow
-go run ./scripts/lib/coverage/
+go run github.com/codetreker/go-cov/cmd/go-cov@v0.1.0 --skip-result-packages tests/
